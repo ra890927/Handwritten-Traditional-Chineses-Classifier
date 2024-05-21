73d2f3924d0c7645b4b24db3934e587bb1a5f0f4
ra890927 <happy.890927410@gmail.com>
2024-05-21 15:39:54 +0800

    feat: add transformation

--- dataset.py
+++ dataset.py
@@ -1,3 +1,4 @@
+import torch
 import numpy as np
 import pandas as pd
 from PIL import Image
@@ -10,11 +11,21 @@ from typing import Tuple, List, Union
 
 
 class HCCRDataset(Dataset):
-    def __init__(self, fp: Union[str, Path], alphabet: str) -> None:
+    def __init__(self, mode: str, fp: Union[str, Path], alphabet: str) -> None:
+        self.mode = mode
         self.alphabet = alphabet
         self.data = self.__transform_df_to_data(fp)
 
+        self.lb = preprocessing.LabelBinarizer()
+        self.lb.fit([i for i in range(len(self.alphabet))])
+
         self.transforms = transforms.Compose([
+            transforms.RandomRotation(degrees=30),
+            transforms.Resize((256, 256)),
+            transforms.RandomCrop((224, 224))
+        ])
+
+        self.to_tensor = transforms.Compose([
             transforms.Resize([224, 224]),
             transforms.ToTensor(),
         ])
@@ -24,7 +35,11 @@ class HCCRDataset(Dataset):
 
     def __getitem__(self, index: int) -> Tuple[Tensor, List[int]]:
         img_path, label = self.data[index]
-        img = self.transforms(Image.open(img_path))
+        img = Image.open(img_path)
+        if self.mode == 'train':
+            img = self.transforms(img)
+        img = self.to_tensor(img)
+        label = np.array(self.lb.transform([label]).ravel())
         return img, label
 
     def __transform_df_to_data(self, fp: Union[str, Path]) -> List[Tuple[str, np.array]]:
@@ -32,14 +47,11 @@ class HCCRDataset(Dataset):
         char_to_index = {c: i for i, c in enumerate(self.alphabet)}
         df['label_index'] = df['label'].map(char_to_index)
 
-        lb = preprocessing.LabelBinarizer()
-        lb.fit([i for i in range(len(self.alphabet))])
-
         ret = []
         for _, row in df.iterrows():
             ret.append((
                 row['image'],
-                np.array(lb.transform([row['label_index']]))
+                row['label_index']
             ))
 
         return ret
